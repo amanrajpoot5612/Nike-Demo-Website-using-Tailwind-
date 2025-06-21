5d31feb56bf8be3d57bd95af2c9ef1f94f2e7a0f
Aman Rajpoot <amanrajpoot5612@gmail.com>
2025-06-22 02:00:40 +0530

    feat: implement main layout and add sections for Hero, Products, Services, and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
+import { CustomerReviews , Hero , Footer ,
+   PopularProducts, SpecialOffer , Services , SuperQuality , Subscribe
+ } from "./sections"
+ import Nav from "./components/Nav"
+
+
 function App() {
 
   return (
     <main className="relative">
-      Nav
+      <Nav/>
       <section className="xl:padding-1
       wide:padding-r padding-b">
-        Hero
+        <Hero/>
       </section>
       <section className="padding">
-        PopularProducts
+        <PopularProducts/>
       </section>
       <section className="padding">
-        SuperQuality
+        <SuperQuality/>
       </section>
       <section className="padding-x py-10">
-        Servics
+        <Services/>
       </section>
       <section className="padding">
-        SpecialOffer
+        <SpecialOffer/>
+      </section>
+      <section className="padding
+      bg-pale-blue">
+        <CustomerReviews/>
+      </section>
+      <section className="padding-x
+      sm:py-32 py-16 w-full">
+        <Subscribe/>
+      </section>
+      <section className="padding-x
+      bg-black padding-t pb-8">
+        <Footer/>
       </section>
     </main>
   )
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffer,
+    Subscribe,
+    Services,
+    Footer,
+    CustomerReviews
+}
